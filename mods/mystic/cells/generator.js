069070e252d7f31d314672a882c991d741c913d8
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-10-25 12:28:57 +0800

    @Squeakmeow, pls find bug or else ban

--- mods/mystic/cells/generator.js
+++ mods/mystic/cells/generator.js
@@ -6,12 +6,14 @@ export default class Generator extends Cell {
     static type = "generator";
     static texture = "./mods/mystic/tex/generator.png";
     update() {
+        let pos = this.posAtRot(this.rot & 3);
+        if (pos[0] < 0 || pos[0] >= this.sys.w || pos[1] < 0 || pos[1] >= this.sys.h) return;
         let toGen = this.cellAtRot((this.rot + 2) & 3);
-        
+
         if (toGen) {
             let pushCell = this.cellAtRot();
             if (!pushCell || pushCell.push(this.rot, 1)) {
-                this.sys.addCell(toGen.constructor, ...this.posAtRot(this.rot), toGen.rot);
+                this.sys.addCell(toGen.constructor, ...pos, toGen.rot);
             }
         }
     }
